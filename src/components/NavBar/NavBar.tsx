1ecaf3ad01af8a8115a4f8829cd9f5135fde08d2
bengaudry <bengaudry@outlook.fr>
2023-03-17 13:55:34 +0100

    Better navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,8 +2,8 @@ import { NavBarElement } from "./components/NavLink";
 
 export function NavBar() {
   return (
-    <nav className="fixed bottom-0 z-30 w-full bg-slate-800 py-3 pb-8 border-t border-slate-600">
-      <ul className="flex flex-row items-center justify-between px-10 h-full">
+    <nav className="fixed bottom-0 z-30 w-full bg-slate-800 py-3 pb-10 border-t border-slate-600">
+      <ul className="flex flex-row items-center justify-between px-16 h-full">
         <NavBarElement icon="apps" label="Home" to="/" />
         <NavBarElement icon="add" label="Add" to="/add" />
         <NavBarElement icon="user" label="Profile" to="/profile" />
