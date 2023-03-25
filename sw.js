54448d11fc49beab88e62b225d9f84830a869a8c
bengaudry <bengaudry@outlook.fr>
2023-03-25 20:07:48 +0100

    Added a service worker

---
+++ sw.js
@@ -0,0 +1,33 @@
+const VERSION = "v3";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+  console.log(`[${VERSION}] installing`);
+});
+
+self.addEventListener("activate", () => {
+  clients.claim();
+  console.log(`[${VERSION}] active`);
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(`[${VERSION}] Fetching ${event.request.url}`);
+  console.log(`[${VERSION}] Mode ${event.request.mode}`);
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          return await fetch(event.request);
+        } catch (e) {
+          return new Response("Heyy");
+        }
+      })()
+    );
+  }
+});
