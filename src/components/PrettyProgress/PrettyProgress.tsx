a8ebc99420ee8cb4dcd589e721a50fbbed51cdf3
bengaudry <bengaudry@outlook.fr>
2023-03-31 18:43:38 +0200

    Upgraded progressbar

--- src/components/PrettyProgress/PrettyProgress.tsx
+++ src/components/PrettyProgress/PrettyProgress.tsx
@@ -4,14 +4,13 @@ export function PrettyProgress(props: { percent: number; className?: string }) {
   return (
     <div className="mt-8 relative w-full h-8">
       <div className="relative inset-0 grid grid-cols-3 gap-8 w-ful h-8">
-        <div
-          className="h-0.5 bg-neutral-700 rounded-full overflow-hidden absolute top-1/2 m-auto w-full"
-          style={{ width: "calc(100%- 2 * 2rem)" }}
-        >
-          <div
-            className={`h-full bg-violet-500`}
-            style={{ width: `${props.percent}%` }}
-          ></div>
+        <div className="px-8 w-full absolute top-1/2">
+          <div className="h-0.5 bg-neutral-700 rounded-full overflow-hidden">
+            <div
+              className={`h-full bg-violet-500`}
+              style={{ width: `${props.percent}%` }}
+            ></div>
+          </div>
         </div>
         <div className="w-full flex flex-row justify-between absolute">
           <Number active>1</Number>
