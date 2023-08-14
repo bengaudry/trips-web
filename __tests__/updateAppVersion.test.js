692135f50cd7bae9ae5c0d8715627c75d509d4a5
bengaudry <bengaudry@outlook.fr>
2023-08-14 18:28:22 +0200

    App version is handled

---
+++ __tests__/updateAppVersion.test.js
@@ -0,0 +1,30 @@
+import { test, expect } from "vitest";
+import { generateNewVersion } from "./updateAppVersion";
+
+test("0.0.0", () => {
+  expect(generateNewVersion("0.0.0")).toBe("0.0.1");
+});
+
+test("0.0.9", () => {
+  expect(generateNewVersion("0.0.9")).toBe("0.1.0");
+});
+
+test("0.9.0", () => {
+  expect(generateNewVersion("0.9.0")).toBe("0.9.1");
+});
+
+test("0.9.9", () => {
+  expect(generateNewVersion("0.9.9")).toBe("1.0.0");
+});
+
+test("1.9.9", () => {
+  expect(generateNewVersion("1.9.9")).toBe("2.0.0");
+});
+
+test("1.1.1", () => {
+  expect(generateNewVersion("1.1.1")).toBe("1.1.2");
+});
+
+test("9.9.9", () => {
+  expect(generateNewVersion("9.9.9")).toBe("10.0.0");
+});
---
+++ updateAppVersion.mjs
@@ -0,0 +1,34 @@
+import { writeFileSync } from "fs";
+import { APP_VERSION } from "./src/lib/constants/appVersion.js";
+
+const versionFilePath = "./src/lib/constants/appVersion.js";
+
+export function generateNewVersion(oldVersion) {
+  const nbs = oldVersion.split(".");
+
+  let one = parseInt(nbs[0]);
+  let two = parseInt(nbs[1]);
+  let three = parseInt(nbs[2]);
+
+  if (three >= 9) {
+    three = 0;
+
+    if (two >= 9) {
+      two = 0;
+      one++;
+    } else {
+      two++;
+    }
+  } else {
+    three++;
+  }
+
+  return `${one}.${two}.${three}`;
+}
+
+const newVersion = generateNewVersion(APP_VERSION);
+
+const content = `export const APP_VERSION = "${newVersion}";\n`;
+
+writeFileSync(versionFilePath, content, "utf-8");
+console.log(`Version updated to ${newVersion} in ${versionFilePath}`);
