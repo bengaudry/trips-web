4c39a0a82dc06f2b09ecde44584c683e97f7587d
bengaudry <bengaudry@outlook.fr>
2023-08-18 19:49:07 +0200

    Fix: margin bottom in app that made a blank space

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -24,7 +24,7 @@ export function Layout() {
   }, []);
 
   return (
-    <div className="App dark:text-white dark:bg-grayblue-900 mb-16 m-auto lg:pl-72">
+    <div className="App dark:text-white dark:bg-grayblue-900 m-auto lg:pl-72">
       <NavBar />
       <div className="max-w-screen-md mx-auto">
         <NotStandaloneAlert
