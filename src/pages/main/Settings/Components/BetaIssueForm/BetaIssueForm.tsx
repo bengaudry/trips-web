5bdf360577fd03819eb1f0986e469000a4968442
bengaudry <bengaudry@outlook.fr>
2023-09-01 16:46:30 +0200

    Feat: Submit issue to dev as beta tester

---
+++ src/pages/main/Settings/Components/BetaIssueForm/BetaIssueForm.tsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import { Cta } from "../../../../../components";
+import { Input } from "../../../../../components/form";
+import type { IssueCategory } from "../BetaPage/BetaPage";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirebaseApp, getFirebaseAuth } from "../../../../../../server";
+import { strTruish } from "../../../../../lib/functions";
+
+export async function addIssue(content: {
+  category: IssueCategory | string;
+  otherCategory?: string;
+  title: string;
+  description: string;
+  issueUrl?: string;
+}) {
+  const db = getFirestore(getFirebaseApp());
+  const tripsCollection = collection(db, "/betaIssues");
+
+  await addDoc(tripsCollection, {
+    ...content,
+    testerEmail: getFirebaseAuth().currentUser?.email,
+  })
+    .then(() => {
+      window.location.href = "/settings";
+    })
+    .catch((err) => {
+      console.log("Firebase error :", err);
+      alert(
+        `Error while sending to the database, please contact us. (Error: ${err})`
+      );
+      return false;
+    });
+
+  return true;
+}
+
+export function BetaIssueForm() {
+  const [issueCategory, setIssueCategory] = useState<IssueCategory>("auth");
+  const [otherIssueCategory, setOtherIssueCategory] = useState("");
+  const [issueTitle, setIssueTitle] = useState("");
+  const [issueDesc, setIssueDesc] = useState("");
+  const [issueUrl, setIssueUrl] = useState("");
+
+  const handleSubmitIssue = () => {
+    if (
+      strTruish(issueTitle) &&
+      strTruish(issueCategory) &&
+      strTruish(issueDesc)
+    ) {
+      addIssue({
+        category:
+          issueCategory === "other"
+            ? `Other : ${otherIssueCategory}`
+            : issueCategory,
+        title: issueTitle,
+        description: issueDesc,
+        issueUrl: issueUrl,
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      Issue type
+      <select
+        className="bg-grayblue-900 w-full py-2 px-4 rounded-lg"
+        onChange={(e) => setIssueCategory(e.target.value as IssueCategory)}
+      >
+        <option value="auth" selected>
+          Authentication
+        </option>
+        <option value="code">Code</option>
+        <option value="theme">Dark / Light mode</option>
+        <option value="data">Data</option>
+        <option value="slowness">Slowness</option>
+        <option value="translation">Translation</option>
+        <option value="missing-or-misplaced-element">
+          Missing or misplaced element
+        </option>
+        <option value="other">Other</option>
+      </select>
+      {issueCategory === "other" && (
+        <Input
+          name="Other category"
+          type="text"
+          required
+          placeholder="What category would you put this issue in ?"
+          value={otherIssueCategory}
+          onChange={(e) => setOtherIssueCategory(e.target.value)}
+        />
+      )}
+      <div className="flex flex-col gap-4">
+        <Input
+          name="title"
+          type="text"
+          value={issueTitle}
+          onChange={(e) => setIssueTitle(e.target.value)}
+          placeholder="How would you name this issue ?"
+          required
+        />
+        <label>
+          Description *
+          <textarea
+            placeholder="Describe as precisely as possible the issue you faced while using the app"
+            value={issueDesc}
+            onChange={(e) => setIssueDesc(e.target.value)}
+            className="bg-transparent border-2 border-grayblue-700 rounded-lg w-full h-36 resize-none outline-none px-6 py-3 placeholder:text-grayblue-500 focus:border-brand-500"
+          />
+        </label>
+        <Input
+          name="url"
+          type="text"
+          value={issueUrl}
+          onChange={(e) => setIssueUrl(e.target.value)}
+          placeholder="The url where the issue poped (optionnal)"
+        />
+        <Cta type="button" className="mt-4" onClick={handleSubmitIssue}>
+          Submit issue
+        </Cta>
+      </div>
+    </form>
+  );
+}
--- src/pages/main/Settings/Components/BetaPage/BetaPage.tsx
+++ src/pages/main/Settings/Components/BetaPage/BetaPage.tsx
@@ -1,21 +1,20 @@
 import { useState } from "react";
 import { getFirebaseAuth } from "../../../../../../server";
-import { Cta, Modal, SecondaryText } from "../../../../../components";
-import { Input, Select } from "../../../../../components/form";
+import { Modal, SecondaryText } from "../../../../../components";
+import { BetaIssueForm } from "../BetaIssueForm/BetaIssueForm";
 
-type IssueCategory =
+export type IssueCategory =
   | "auth"
   | "code"
   | "theme"
   | "data"
   | "slowness"
   | "translation"
-  | "other";
+  | "other"
+  | "missing-or-misplaced-element";
 
 export function BetaPage() {
   const [modalShown, setModalShown] = useState(false);
-  const [issueCategory, setIssueCategory] = useState<IssueCategory>("auth");
-  const [otherIssueCategory, setOtherIssueCategory] = useState("");
 
   return (
     <>
@@ -43,54 +42,7 @@ export function BetaPage() {
         isShown={modalShown}
         title="Report an issue"
       >
-        Issue type
-        <select
-          className="bg-grayblue-900 w-full py-2 px-4 rounded-lg"
-          onChange={(e) => setIssueCategory(e.target.value as IssueCategory)}
-        >
-          <option value="auth" selected>
-            Authentication
-          </option>
-          <option value="code">Code</option>
-          <option value="theme">Dark / Light mode</option>
-          <option value="data">Data</option>
-          <option value="slowness">Slowness</option>
-          <option value="translation">Translation</option>
-          <option value="other">Other</option>
-        </select>
-        {issueCategory === "other" && (
-          <Input
-            name="Other category"
-            type="text"
-            required
-            placeholder="What category would you put this issue in ?"
-            value={otherIssueCategory}
-            onChange={(e) => setOtherIssueCategory(e.target.value)}
-          />
-        )}
-        <div className="flex flex-col gap-4">
-          <Input
-            name="title"
-            type="text"
-            placeholder="How would you name this issue ?"
-            required
-          />
-          <label>
-            Description *
-            <textarea
-              placeholder="Describe as precisely as possible the issue you faced while using the app"
-              className="bg-transparent border-2 border-grayblue-700 rounded-lg w-full h-36 resize-none outline-none px-6 py-3 placeholder:text-grayblue-500 focus:border-brand-500"
-            />
-          </label>
-          <Input
-            name="url"
-            type="text"
-            placeholder="The url where the issue poped (optionnal)"
-          />
-          <Cta type="button" className="mt-4">
-            Submit issue
-          </Cta>
-        </div>
+        <BetaIssueForm />
       </Modal>
     </>
   );
