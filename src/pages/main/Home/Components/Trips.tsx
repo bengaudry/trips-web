260bc06d42c52be8727653c3686baff340bbead9
bengaudry <bengaudry@outlook.fr>
2024-01-09 11:52:47 +0100

    Fix: Modal that opened in a small box on homepage

--- src/pages/main/Home/Components/Trips.tsx
+++ src/pages/main/Home/Components/Trips.tsx
@@ -14,86 +14,9 @@ import { toast } from "react-toastify";
 export function Trips(props: {
   data?: Array<Trip>;
   onDataChange: (newData: Array<Trip>) => void;
+  onOpenTripDetails: (trip: Trip, key: number) => void;
 }) {
   const { t } = useTranslation();
-
-  const [modalOpened, setModalOpened] = useState(false);
-  const [modalContent, setModalContent] = useState<{
-    trip: Trip;
-    key: number;
-  }>();
-
-  // Disables scroll while popup is opened
-  useEffect(() => {
-    document.body.style.overflow = modalOpened ? "hidden" : "auto";
-  }, [modalOpened]);
-
-  const [roundTrip, setRoundTrip] = useState<boolean | undefined>(
-    modalContent?.trip.roundTrip
-  );
-
-  useEffect(() => {
-    setRoundTrip(modalContent?.trip.roundTrip);
-  }, [modalContent]);
-
-  const handleDeleteTrip = () => {
-    if (confirm("Do you really want to delete this trip ?")) {
-      // Checking if the modal contains an id to avoid errors
-      if (modalContent?.trip.id) {
-        // Deletes the trip from the database
-        deleteDoc(doc(getFirebaseDb(), "/trips", modalContent.trip.id))
-          .then(() => {
-            // Deletes the trip from cache and closes the modal if there is cache
-            if (props.data) {
-              let newData = removeElementAtIndex(
-                [...props.data],
-                modalContent.key
-              );
-              props.onDataChange(newData);
-              setModalOpened(false);
-            } else {
-              // Refetch the data if there is no cache
-              window.location.reload();
-            }
-          })
-          .catch((err) => {
-            toast(
-              `Error while deleting trip. Please try again. (Code: ${err.code})`,
-              { type: "error" }
-            );
-          });
-      } else {
-        toast(`Error while deleting trip. Please contact us.`, {
-          type: "error",
-        });
-      }
-    }
-  };
-
-  const handleRoundTripChange = (val: boolean) => {
-    setRoundTrip(val);
-    let ref = doc(getFirebaseDb(), "/trips", modalContent?.trip.id as string);
-    console.log("id", modalContent?.trip.id);
-    updateDoc(ref, { roundTrip: val })
-      .then(() => {
-        if (props.data && modalContent?.key !== undefined) {
-          let newData = [...props.data];
-          newData[modalContent.key] = {
-            ...newData[modalContent.key],
-            roundTrip: val,
-          };
-          props.onDataChange(newData);
-        } else {
-          window.location.reload();
-        }
-      })
-      .catch((err) =>
-        toast(
-          `Error while editing your trip. Please try again. (Code: ${err.code})`,
-          { type: "error" }
-        )
-      );
-  };
 
   return (
     <div className="pt-3">
@@ -104,41 +27,10 @@ export function Trips(props: {
               {...trip}
               showMoreOptBtn
               setModalOpened={(val: boolean) => {
-                setModalOpened(val);
-                setModalContent({ trip: trip, key: key });
+                props.onOpenTripDetails(trip, key);
               }}
             />
           ))}
-          <Modal
-            visible={modalOpened}
-            setVisible={setModalOpened}
-            title={
-              <>
-                <span>{capitalizeString(modalContent?.trip.from)}</span>
-                {modalContent?.trip.roundTrip ? (
-                  <i className="fi fi-rr-exchange inline-block mx-3 translate-y-0.5"></i>
-                ) : (
-                  <i className="fi fi-rr-arrow-right inline-block mx-3 translate-y-0.5"></i>
-                )}
-                <span>{capitalizeString(modalContent?.trip.to)}</span>
-              </>
-            }
-          >
-            <Text.Secondary className="text-lg mb-4">
-              {modalContent?.trip.length}kms - {modalContent?.trip.duration}min
-              <br />
-              {modalContent?.trip.date &&
-                new Date(modalContent.trip.date).toLocaleDateString()}
-            </Text.Secondary>
-            <Checkbox
-              name={t("addpage.inputs.labels.roundtrip")}
-              checked={!!roundTrip}
-              setChecked={handleRoundTripChange}
-            />
-            <Cta type="button" color="danger" onClick={handleDeleteTrip}>
-              Delete trip
-            </Cta>
-          </Modal>
         </div>
       ) : (
         <>
--- src/pages/main/Home/Home.tsx
+++ src/pages/main/Home/Home.tsx
@@ -1,14 +1,28 @@
 import { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { getFirebaseDb } from "../../../../server";
 
-import { calculateDataForStats } from "@/lib/functions";
+import {
+  calculateDataForStats,
+  capitalizeString,
+  removeElementAtIndex,
+} from "@/lib/functions";
 import { Trip } from "@/types";
 
 import { CurrentUser } from "@/api";
 import {
+  Cta,
   Modal,
   NotVerifiedEmailPopup,
   PageLayout,
@@ -20,6 +34,8 @@ import { AnimatePresence, motion } from "framer-motion";
 import { SetUserNameModal } from "./Components/SetUserNameModal";
 import { Stats } from "./Components/Stats";
 import { Trips } from "./Components/Trips";
+import { Checkbox } from "@/components/form";
+import { toast } from "react-toastify";
 
 function fetchCachedTrips() {
   const data = localStorage.getItem("cached-trips-data");
@@ -38,7 +54,6 @@ function setTripsInCache(data?: Trip[]) {
 
 export function Home() {
   const { t } = useTranslation();
-  const { openModal } = useModal();
 
   const [trips, setTrips] = useState<Trip[] | undefined>(fetchCachedTrips());
   const [currentPanel, setCurrentPanel] = useState<0 | 1>(0);
@@ -91,7 +106,85 @@ export function Home() {
 
   const memoizedData = useMemo(() => trips, [trips]);
   const allTrips = memoizedData ? memoizedData : trips ? trips : [];
-  
+
+  const [modalOpened, setModalOpened] = useState(false);
+  const [modalContent, setModalContent] = useState<{
+    trip: Trip;
+    key: number;
+  }>();
+
+  // Disables scroll while popup is opened
+  useEffect(() => {
+    document.body.style.overflow = modalOpened ? "hidden" : "auto";
+  }, [modalOpened]);
+
+  const [roundTrip, setRoundTrip] = useState<boolean | undefined>(
+    modalContent?.trip.roundTrip
+  );
+
+  useEffect(() => {
+    setRoundTrip(modalContent?.trip.roundTrip);
+  }, [modalContent]);
+
+  const handleDeleteTrip = () => {
+    if (confirm("Do you really want to delete this trip ?")) {
+      // Checking if the modal contains an id to avoid errors
+      if (modalContent?.trip.id) {
+        // Deletes the trip from the database
+        deleteDoc(doc(getFirebaseDb(), "/trips", modalContent.trip.id))
+          .then(() => {
+            // Deletes the trip from cache and closes the modal if there is cache
+            if (memoizedData) {
+              let newData = removeElementAtIndex(
+                [...memoizedData],
+                modalContent.key
+              );
+              setTrips(newData);
+              setModalOpened(false);
+            } else {
+              // Refetch the data if there is no cache
+              window.location.reload();
+            }
+          })
+          .catch((err) => {
+            toast(
+              `Error while deleting trip. Please try again. (Code: ${err.code})`,
+              { type: "error" }
+            );
+          });
+      } else {
+        toast(`Error while deleting trip. Please contact us.`, {
+          type: "error",
+        });
+      }
+    }
+  };
+
+  const handleRoundTripChange = (val: boolean) => {
+    setRoundTrip(val);
+    let ref = doc(getFirebaseDb(), "/trips", modalContent?.trip.id as string);
+    console.log("id", modalContent?.trip.id);
+    updateDoc(ref, { roundTrip: val })
+      .then(() => {
+        if (memoizedData && modalContent?.key !== undefined) {
+          let newData = [...memoizedData];
+          newData[modalContent.key] = {
+            ...newData[modalContent.key],
+            roundTrip: val,
+          };
+          setTrips(newData);
+        } else {
+          window.location.reload();
+        }
+      })
+      .catch((err) =>
+        toast(
+          `Error while editing your trip. Please try again. (Code: ${err.code})`,
+          { type: "error" }
+        )
+      );
+  };
+
   return (
     <PageLayout key="home">
       <NotVerifiedEmailPopup className="mb-4" />
@@ -105,6 +198,37 @@ export function Home() {
           <SetUserNameModal setUserNameUnset={setUserNameUnset} />
         </Modal>
       )}
+
+      <Modal
+        visible={modalOpened}
+        setVisible={setModalOpened}
+        title={
+          <>
+            <span>{capitalizeString(modalContent?.trip.from)}</span>
+            {modalContent?.trip.roundTrip ? (
+              <i className="fi fi-rr-exchange inline-block mx-3 translate-y-0.5"></i>
+            ) : (
+              <i className="fi fi-rr-arrow-right inline-block mx-3 translate-y-0.5"></i>
+            )}
+            <span>{capitalizeString(modalContent?.trip.to)}</span>
+          </>
+        }
+      >
+        <Text.Secondary className="text-lg mb-4">
+          {modalContent?.trip.length}kms - {modalContent?.trip.duration}min
+          <br />
+          {modalContent?.trip.date &&
+            new Date(modalContent.trip.date).toLocaleDateString()}
+        </Text.Secondary>
+        <Checkbox
+          name={t("addpage.inputs.labels.roundtrip")}
+          checked={!!roundTrip}
+          setChecked={handleRoundTripChange}
+        />
+        <Cta type="button" color="danger" onClick={handleDeleteTrip}>
+          Delete trip
+        </Cta>
+      </Modal>
 
       <header>
         <Text.Secondary className="text-xl mt-1">
@@ -151,6 +275,10 @@ export function Home() {
                 data={memoizedData}
                 onDataChange={(newData) => {
                   setTrips(newData);
+                }}
+                onOpenTripDetails={(trip, key) => {
+                  setModalOpened(true);
+                  setModalContent({ trip: trip, key: key });
                 }}
               />
             </motion.div>
